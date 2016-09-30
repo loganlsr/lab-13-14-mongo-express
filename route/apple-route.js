878f356c5bde2d3eb49bd5d7dab622711c786c59
Logan Rogers <logan.s.rogers@biola.edu>
2016-09-30 11:32:09 -0700

    Two PUT tests failing, everything else passes

--- route/apple-route.js
+++ route/apple-route.js
@@ -2,6 +2,7 @@
 
 const Router = require('express').Router;
 const jsonParser = require('body-parser').json();
+const createError = require('http-errors');
 
 const List = require('../model/list.js');
 
@@ -11,4 +12,25 @@ appleRouter.post('/api/list/:listID/apple', jsonParser, function(req, res, next)
   List.findByIdAndAddApple(req.params.listID, req.body)
   .then(apple => res.json(apple))
   .catch(next);
+});
+
+appleRouter.get('/api/list/:listID/apple', function(req, res, next){
+  List.findById(req.params.listID)
+  .then(apple => res.json(apple))
+  .catch(next);
+});
+
+appleRouter.delete('/api/list/:listID/apple', function(req, res, next){
+  List.findByIdAndRemove(req.params.id)
+  .then(() => res.status(204))
+  .catch(err => next(createError(404, err.message)));
+});
+
+appleRouter.put('/api/list/:listID/apple', jsonParser, function(req, res, next){
+  List.findByIdAndUpdate(req.params.id, req.body, {new: true})
+  .then(list => res.json(list))
+  .catch(err => {
+    if (err.name === 'ValidationError') return next(err);
+    next(createError(404, err.message));
+  });
 });
--- test/apple-router-test.js
+++ test/apple-router-test.js
@@ -24,6 +24,7 @@ const exampleList = {
 };
 
 describe('testing apple routes', function(){
+
   describe('testing POST requests', function(){
     describe('with valid list id and appleBody', function(){
 
@@ -84,6 +85,200 @@ describe('testing apple routes', function(){
         .set('Content-Type', 'application/json')
         .end((err, res) => {
           expect(res.status).to.equal(400);
+          done();
+        });
+      });
+    });
+  });
+
+  describe('testing GET requests', function(){
+    describe('with valid list id and appleBody', function(){
+
+      before(done => {
+        new List(exampleList).save()
+        .then( list => {
+          this.tempList = list;
+          done();
+        })
+        .catch(done);
+      });
+
+      after(done => {
+        Promise.all([
+          List.remove({}),
+          Apple.remove({}),
+        ])
+        .then(() => done())
+        .catch(done);
+      });
+
+      it('should return an apple', done => {
+        request.get(`${url}/api/list/${this.tempList.id}/apple`)
+        .end((err, res) => {
+          expect(res.status).to.equal(200);
+          expect(res.body.type).to.equal(exampleApple.type);
+          expect(res.body.color).to.equal(exampleApple.color);
+          expect(res.body.size).to.equal(exampleApple.size);
+          done();
+        });
+      });
+    });
+
+    describe('with an invalid list id and appleBody', function(){
+
+      before(done => {
+        new List(exampleList).save()
+        .then( list => {
+          this.tempList = list;
+          done();
+        })
+        .catch(done);
+      });
+
+      after(done => {
+        Promise.all([
+          List.remove({}),
+          Apple.remove({}),
+        ])
+        .then(() => done())
+        .catch(done);
+      });
+
+      it('should return error code 404', done => {
+        request.get(`${url}/api/list/${this.tempList.id}/no-apple`)
+        .end((err, res) => {
+          expect(res.status).to.equal(404);
+          done();
+        });
+      });
+    });
+  });
+
+  describe('testing DELETE requests', function() {
+
+    describe('with valid id', function() {
+
+      before( done => {
+        new List(exampleList).save()
+        .then( list => {
+          this.tempList = list;
+          done();
+        })
+        .catch(done);
+      });
+
+      it('should delete an item and give status code 204', done => {
+        request.delete(`${url}/api/list/${this.tempList.id}/apple`)
+        .end((err, res) => {
+          if (err) return done(err);
+          expect(res.status).to.equal(204);
+          done();
+        });
+        done();
+      });
+    });
+
+    describe('for a file that does not exist', function() {
+
+      before( done => {
+        new List(exampleList).save()
+        .then( list => {
+          this.tempList = list;
+          done();
+        })
+        .catch(done);
+      });
+
+      it('should give error code 404 not found', done => {
+        request.delete(`${url}/api/list/${this.tempList.id}/no-apple`)
+        .end((err, res) => {
+          expect(res.status).to.equal(404);
+          done();
+        });
+      });
+    });
+  });
+
+  describe('testing PUT requests', function() {
+
+    describe('with valid id', function(){
+
+      before( done => {
+        new List(exampleList).save()
+        .then( list => {
+          this.tempList = list;
+          done();
+        })
+        .catch(done);
+      });
+
+      after( done => {
+        if(this.tempList){
+          List.remove({})
+          .then(() => done())
+          .catch(done);
+          return;
+        }
+        done();
+      });
+
+      it('should return a list with a status 200', done => {
+        let updateData = {type:'bad-apple', color: 'black', size: 'mini'};
+        request.put(`${url}/api/list/${this.tempList.id}/apple`)
+        .send(updateData)
+        .end((err, res) => {
+          expect(res.status).to.equal(200);
+          expect(res.body.type).to.equal(updateData.type);
+          expect(res.body.color).to.equal(updateData.color);
+          expect(res.body.size).to.equal(updateData.size);
+          let timestamp = new Date(res.body.timestamp);
+          expect(timestamp.toString()).to.equal(exampleList.timestamp.toString());
+          this.tempList = res.body;
+          done();
+        });
+      });
+    });
+
+    describe('with no body provided', function() {
+
+      before( done => {
+        exampleList.timestamp = new Date();
+        new List(exampleList).save()
+        .then( list => {
+          this.tempList = list;
+          done();
+        })
+        .catch(done);
+      });
+
+      after( done => {
+        if(this.tempList) {
+          List.remove({})
+          .then(() => done())
+          .catch(done);
+          return;
+        }
+        done();
+      });
+
+      it('should return a 400 bad request', done => {
+        let updateData = 'good day';
+        request.put(`${url}/api/list/${this.tempList.id}/apple`)
+        .set('Content-Type', 'application/json')
+        .send(updateData)
+        .end((err, res) => {
+          expect(res.status).to.equal(400);
+          done();
+        });
+      });
+    });
+
+    describe('with no body', function() {
+
+      it('should 404 not found', done => {
+        request.put(`${url}/api/list/${this.tempList.id}/no-apple`)
+        .end((err, res) => {
+          expect(res.status).to.equal(404);
           done();
         });
       });
